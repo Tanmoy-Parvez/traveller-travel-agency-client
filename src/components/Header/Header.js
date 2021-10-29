85100a0bec6e1bfc46a38d0878a75c254da126e9
Siam-Parvez <tanmoyparvez23@gmail.com>
2021-10-29 11:45:43 +0600

    nav menus added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import { Container, Nav, Navbar, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import logo from '../../images/logo.png'
 import "./Header.css"
 
 const Header = () => {
     // const { user, logOut } = useAuth(); // context api
-    // const history = useHistory();
+    const history = useHistory();
 
-    // // change route options
-    // const handleLogIn = () => {
-    //     history.push("/login")
-    // }
-
-    // const handleSignUp = () => {
-    //     history.push("/signup")
-    // }
+    // change route options
+    const handleSignIn = () => {
+        history.push("/signIn")
+    }
 
     // const handleNavbarBand = () => {
     //     history.push("/home")
@@ -39,14 +35,18 @@ const Header = () => {
                     {/* menu options */}
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ms-auto navbar-item">
-                            <Nav.Item className="nav-item">
+                        <Nav className="ms-auto">
+                            <Nav.Item class="d-flex align-items-center navbar-item">
                                 <Nav.Link>
                                     <Link to="/home">Home</Link>
+                                    <Button onClick={handleSignIn} variant="info" className="mx-3 rounded-pill px-4 ">Sign in</Button>
                                 </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <span className="fw-bolder ms-3">User Name</span>
+                                <Nav.Link>
+                                    <Link to="/home">My Bookings</Link>
+                                    <Link to="/home">Manage Tours</Link>
+                                    <Link to="/home">Add Tour</Link>
+                                    <span className="fw-bolder ms-3">User Name</span>
+                                </Nav.Link>
                                 <Button variant="danger" className="mx-3 rounded-pill px-4 ">Log Out <i className="fas fa-sign-out-alt"></i></Button>
                             </Nav.Item>
                         </Nav>
