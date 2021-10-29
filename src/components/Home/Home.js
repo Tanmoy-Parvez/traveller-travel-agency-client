50052c0ecfd860645d379bf3f6415e72f37183c6
Siam-Parvez <tanmoyparvez23@gmail.com>
2021-10-29 11:08:05 +0600

    extra part2 added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import News from '../News/News';
+import Subscribe from '../Subscribe/Subscribe';
 import Tours from '../Tours/Tours';
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner />
             <Tours />
             <News />
+            <Subscribe />
         </div>
     );
 };
